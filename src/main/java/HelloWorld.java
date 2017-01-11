ff14f2450bba44ef8b4d31cb758c1278198e4408
Duc Nguyen <nguyen@emersion.fr>
2017-01-11 16:21:16 +0100

    Adding hello world

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,7 +1,11 @@
-package PACKAGE_NAME;
-
 /**
  * Created by nomeji on 1/11/17.
  */
+
+import static spark.Spark.*;
+
 public class HelloWorld {
+    public static void main(String[] args) {
+        get("/hello", (req, res) -> "Hello World");
+    }
 }
