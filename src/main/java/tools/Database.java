cf2dda2f4a25e7c387008bc37972a9773090234b
lonaxous <lonaxous.game@hotmail.fr>
2017-05-05 14:36:00 +0200

    correction de bug pour la creation d'event

--- src/main/java/tools/Database.java
+++ src/main/java/tools/Database.java
@@ -439,7 +439,7 @@ public class Database {
         Statement s= co.createStatement();
         ResultSet rs = s.executeQuery(requete);
         while(rs.next()){
-            listEvent.add(new Event(rs.getInt(1),rs.getString(2),rs.getString(3),rs.getString(4)));
+            listEvent.add(new Event(rs.getInt(1),rs.getString(2),rs.getString(3),rs.getString(4),selectOrganiser(rs.getInt(1))));
         }
         return listEvent;
     }
